23e23d4b59878e228bf7073d3bcc5a99146cfede
Jasper Wang <jasperwang50@gmail.com>
2024-06-07 22:59:43 -0400

    New updates

--- folders/urls.py
+++ folders/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path
-from .views import (RootFolderView, FolderCreateView, SubFolderView, FolderDeleteView,
-                    FolderUpdateView,
-                    FolderContentCreateView, FolderContentDeleteView, FolderContentUpdateView)
+# from .views import (RootFolderView, FolderCreateView, SubFolderView, FolderDeleteView,
+#                     FolderUpdateView,
+#                     FolderContentCreateView, FolderContentDeleteView, FolderContentUpdateView)
 
 app_name = 'folders'
 
 urlpatterns = [
-    path('', RootFolderView.as_view(), name='root-folders'),
-    path('<int:pk>/', SubFolderView.as_view(), name='sub-folder'),
-    path('folder/add/', FolderCreateView.as_view(), name='folder-add'),
-    path('folder/<int:parent_id>/add/', FolderCreateView.as_view(), name='subfolder-add'),
-    path('folder/delete/<int:pk>', FolderDeleteView.as_view(), name='root-folder-delete'),
-    path('folder/<int:parent_id>/delete/<int:pk>', FolderDeleteView.as_view(), name='folder-delete'),
-    path('folder/update/<int:pk>', FolderUpdateView.as_view(), name='root-folder-update'),
-    path('folder/<int:parent_id>/update/<int:pk>', FolderUpdateView.as_view(), name='folder-update'),
-    path('foldercontent/add/', FolderContentCreateView.as_view(), name='foldercontent-add'),
-    path('foldercontent/<int:parent_id>/add/', FolderContentCreateView.as_view(), name='subfoldercontent-add'),
-    path('foldercontent/delete/<int:pk>/', FolderContentDeleteView.as_view(), name='root-foldercontent-delete'),
-    path('foldercontent/<int:parent_id>/delete/<int:pk>/', FolderContentDeleteView.as_view(), name='foldercontent-delete'),
-    path('foldercontent/update/<int:pk>/', FolderContentUpdateView.as_view(), name='root-foldercontent-update'),
-    path('foldercontent/<int:parent_id>/update/<int:pk>/', FolderContentUpdateView.as_view(), name='foldercontent-update'),
+    # path('', RootFolderView.as_view(), name='root-folders'),
+    # path('<int:pk>/', SubFolderView.as_view(), name='sub-folder'),
+    # path('folder/add/', FolderCreateView.as_view(), name='folder-add'),
+    # path('folder/<int:parent_id>/add/', FolderCreateView.as_view(), name='subfolder-add'),
+    # path('folder/delete/<int:pk>', FolderDeleteView.as_view(), name='root-folder-delete'),
+    # path('folder/<int:parent_id>/delete/<int:pk>', FolderDeleteView.as_view(), name='folder-delete'),
+    # path('folder/update/<int:pk>', FolderUpdateView.as_view(), name='root-folder-update'),
+    # path('folder/<int:parent_id>/update/<int:pk>', FolderUpdateView.as_view(), name='folder-update'),
+    # path('foldercontent/add/', FolderContentCreateView.as_view(), name='foldercontent-add'),
+    # path('foldercontent/<int:parent_id>/add/', FolderContentCreateView.as_view(), name='subfoldercontent-add'),
+    # path('foldercontent/delete/<int:pk>/', FolderContentDeleteView.as_view(), name='root-foldercontent-delete'),
+    # path('foldercontent/<int:parent_id>/delete/<int:pk>/', FolderContentDeleteView.as_view(), name='foldercontent-delete'),
+    # path('foldercontent/update/<int:pk>/', FolderContentUpdateView.as_view(), name='root-foldercontent-update'),
+    # path('foldercontent/<int:parent_id>/update/<int:pk>/', FolderContentUpdateView.as_view(), name='foldercontent-update'),
 ]
