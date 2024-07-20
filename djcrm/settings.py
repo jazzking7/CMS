67906dcf70980572f66f4085f60c4bd8dfe33a58
Jasper Wang <jasperwang50@gmail.com>
2024-07-20 17:24:54 -0400

    env variable for AWS endpoint and bucket name

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -173,8 +173,8 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'tailwind'
 # DigitalOcean Spaces configurations
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'open-api-spaces'
-AWS_S3_ENDPOINT_URL = 'https://urchin-app.nyc3.digitaloceanspaces.com'  
+AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_ENDPOINT_URL = env('AWS_S3_ENDPOINT_URL')
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
